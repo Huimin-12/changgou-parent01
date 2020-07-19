39bd378d72c194674408f25db9cca4e7c6f5271a
huimin <2237501874@qq.com>
2020-07-19 10:26:19 +0800

    分布式id，使用雪花算法

--- changgou_service/changgou_service_goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/GoodsApplication.java
@@ -1,8 +1,11 @@
 package com.changgou;
 
+import com.changgou.util.IdWorker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -11,5 +14,15 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class,args);
+    }
+    //从配置文件当中获取值
+    @Value("${workerId}")
+    private int workerId;
+    @Value("${datacenterId}")
+    private int datacenterId;
+    //注入到spring容器当中，方便使用分布式id
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(workerId,datacenterId);
     }
 }
